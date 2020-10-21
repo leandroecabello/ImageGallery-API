9074bcea376596c78473a0381b47c4e38a8e3260
leandro <leandro.ecabello@gmail.com>
2020-10-21 18:45:55 -0300

    updated cors

--- src/app/app.js
+++ src/app/app.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
-//const cors = require("cors");
-//const corsOptions = require("./libs/cors");
+const cors = require("cors");
 const photosRoutes = require("./routes/photos.routes");
 const app = express();
 
@@ -11,19 +10,7 @@ app.set("port", process.env.PORT || 3000);
 
 // Middlewares
 app
-  .use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
-    );
-    res.header(
-      "Access-Control-Allow-Methods",
-      "GET, POST, OPTIONS, PUT, DELETE"
-    );
-    res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
-    next();
-  })
+  .use(cors())
   .use(morgan("dev"))
   .use(express.json())
   .use(express.urlencoded({ extended: false }));
